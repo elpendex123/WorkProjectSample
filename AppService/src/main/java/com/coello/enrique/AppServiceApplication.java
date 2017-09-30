482c93c18148bb022d8fccf32a50faa70ef59256
Enrique Coello <enrique.coello@gmail.com>
2017-09-29 22:39:16 -0400

    Updated the Git ignore file to exclude build directory and created main class

---
+++ AppService/src/main/java/com/coello/enrique/AppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coello.enrique;
+
+/**
+ * Created by kike on 9/29/17.
+ */
+@SpringBootApplication
+public class AppServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        SpringApplication.run(AppServiceApplication.class, args);
+    }
+}
