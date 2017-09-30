aab0f724fd2ced62a3bf7a6885239de4a2260d1c
Enrique Coello <enrique.coello@gmail.com>
2017-09-29 23:31:28 -0400

    Updated the main app class file to use Spring Boot Servlet Initializer

--- AppService/src/main/java/com/coello/enrique/AppServiceApplication.java
+++ AppService/src/main/java/com/coello/enrique/AppServiceApplication.java
@@ -1,13 +1,14 @@
 package com.coello.enrique;
 
-/**
- * Created by kike on 9/29/17.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class AppServiceApplication {
+public class AppServiceApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         System.out.println("Hello World");
-
         SpringApplication.run(AppServiceApplication.class, args);
+        System.out.println("Bye World");
     }
 }
