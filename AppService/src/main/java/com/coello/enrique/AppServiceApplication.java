a82b6409c0ce44eeddd4f2d588c9690ed15ea896
Enrique Coello <enrique.coello@gmail.com>
2017-09-29 23:51:44 -0400

    Added a custom banner to the main startup class

--- AppService/src/main/java/com/coello/enrique/AppServiceApplication.java
+++ AppService/src/main/java/com/coello/enrique/AppServiceApplication.java
@@ -4,11 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+// SpringBootServletInitializer is an extension of WebApplicationInitializer.
+// It is needed to run a SpringApplication from a WAR deployment.
+// The implementing classes are supposed to provided the source configuration class by extending configure() method.
+// The configuration source class should be annotated with @SpringBootApplication or its variant.
 @SpringBootApplication
 public class AppServiceApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        SpringApplication.run(AppServiceApplication.class, args);
-        System.out.println("Bye World");
+        SpringApplication app = new SpringApplication(AppServiceApplication.class);
+
+        app.setLogStartupInfo(true);
+
+        app.setBanner((environment, sourceClass, out) -> {
+            out.println("MY BANNER");
+            out.println("MY BANNER");
+            out.println("MY BANNER");
+            out.println("MY BANNER");
+            out.println("MY BANNER");
+        });
+
+        app.run(args);
     }
 }
